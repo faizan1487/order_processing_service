eae0c9ae83471b32686a471afb4857870bc2ddee
faizan1487 <faizanahmed14887@gmail.com>
2024-05-26 17:15:05 +0500

    api_getway.conf service discovery nginx file added

--- orders/models.py
+++ orders/models.py
@@ -7,5 +7,6 @@ class Order(models.Model):
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=50, default='Pending')
     created_at = models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
         return f"Order {self.id}"
